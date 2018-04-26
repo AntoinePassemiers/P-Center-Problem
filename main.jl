bb6c00ba1bea42257117c2daf105c48da5429a4f
AntoinePassemiers <apassemi@ulb.ac.be>
2018-04-26 19:00:11 +0200

    CLA parser

--- main.jl
+++ main.jl
@@ -4,6 +4,8 @@
 
 __precompile__()
 
+using ArgParse
+
 include("p1.jl")
 include("p3.jl")
 
@@ -27,31 +29,51 @@ function load_instance(file_path::AbstractString)
     return distances, max_n_centers
 end
 
+function main()
+    arg_settings = ArgParseSettings(description="P-Center Solver: parameters")
 
-distances, p = load_instance(FILE_PATH)
-solver = CbcSolver()
-# model, y = create_p1(distances, p, solver)
-model, y = create_p3(distances, p, solver)
+    @add_arg_table arg_settings begin
+        "filepath"
+            help = "Path to the instance file"
+            required = true
+        "form"
+            help = "Formulation of the P-Center Problem"
+            required = true
+            default = "p1"
+    end
+    parsed_args = parse_args(ARGS, arg_settings)
 
-# Solve p-center problem
-println("Number of variables  : ", MathProgBase.numvar(model))
-println("Number of constraints: ", MathProgBase.numconstr(model))
-println("\nSolving problem...")
-start_time = time()
-status = solve(model)
-exectime = time() - start_time
-println("Status    : $status")
-println("Solve time: $(@sprintf("%.3f", exectime)) s")
+    distances, p = load_instance(FILE_PATH)
+    solver = CbcSolver()
 
-obj = getobjectivevalue(model)
-println("Objective : ", obj)
+    if parsed_args["form"] == "p1"
+        model, y = create_p1(distances, p, solver)
+    elseif parsed_args["form"] == "p3"
+        model, y = create_p3(distances, p, solver)
+    end
 
-# Write solution
-open("out.txt", "w") do f
-    write(f, "Value of the objective function: $obj\n\n")
-    for i=1:length(y)
-        if getvalue(y[i]) > 0
-            write(f, "Center selected at area $i\n")
+    # Solve p-center problem
+    println("Number of variables  : ", MathProgBase.numvar(model))
+    println("Number of constraints: ", MathProgBase.numconstr(model))
+    println("\nSolving problem...")
+    start_time = time()
+    status = solve(model)
+    exectime = time() - start_time
+    println("Status    : $status")
+    println("Solve time: $(@sprintf("%.3f", exectime)) s")
+
+    obj = getobjectivevalue(model)
+    println("Objective : ", obj)
+
+    # Write solution
+    open("out.txt", "w") do f
+        write(f, "Value of the objective function: $obj \n\n")
+        for i=1:length(y)
+            if getvalue(y[i]) > 0
+                write(f, "Center selected at area $i \n")
+            end
         end
     end
 end
+
+main()
